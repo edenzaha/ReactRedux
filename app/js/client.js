41abc0ea2a67f937508ea18a558657c30fcfb77a
edenzaha <eden.net@gmail.com>
2017-06-29 15:12:03 +0300

    more moremo

--- app/js/client.js
+++ app/js/client.js
@@ -1,24 +1,60 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import {Provider} from "react-redux"
-import App from "./components/App"
 import { createStore } from "redux"
-import todoApp from "./reducers/todosReducers"
+import todoApp from "./todoApplication/reducers/todosReducers"
+import PropTypes from 'prop-types'
 
+//root apps
+import App from "./todoApplication/components/App"
+import TestApp from "./testsApplication/components/App"
 
 let store = createStore(todoApp);
-
-
 const app = document.getElementById("app");
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,app);
 
 
 
+const Apps = {
+    TESTS : "Tests",
+    TODO : "Todo"
+}
+
+const AppLink = ({text,onAppClick}) => (
+    <div>
+        <a href="#" onClick={onAppClick}>{text}</a>
+    </div>
+)
 
 
+const Main = () => (
+    <div>        
+        {Object.keys(Apps).map((k,i) => (
+            <AppLink key={i} text={k} onAppClick={()=> AppSwitcher(k)} />
+        ))}
+    </div>
+)
+
+
+
+ReactDOM.render(<Main />, app)
+
+ 
+function AppSwitcher(selectedApp = Apps.TESTS){
+    switch(selectedApp){
+        case "TESTS":
+            ReactDOM.render(<TestApp />,app);
+            break;
+        case "TODO":
+            ReactDOM.render(
+            <Provider store={store}>
+                <App />
+            </Provider>,app);
+            break;
+    }
+}
+
+
+//AppSwitcher(Apps.TODO);
 /*
 STORE ACTION SAMPLES
 import {
---
+++ app/js/testsApplication/components/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+
+const TestApp = () => (
+    <div>
+         TEST APP!
+    </div>
+)
+
+export default TestApp
